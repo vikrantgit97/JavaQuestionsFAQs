02425ec7d8cc670d18d92ef5bbe0704f26b7b1f5
Vikrant <vikrantgopewar@gmail.com>
2023-08-08 22:16:55 +0530

    SwapNumbers.java

---
+++ src/com/test/easy/SwapNumbers.java
@@ -0,0 +1,12 @@
+package com.test.easy;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 78, b = 51;
+        System.out.println("Before swapping -> " + " a : " + a + " & b : " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping -> " + " a : " + a + " & b : " + b);
+    }
+}
