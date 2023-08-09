015a8ca71f75fb4afbef62667934c85dd05970e5
Vikrant <vikrantgopewar@gmail.com>
2023-08-09 23:23:40 +0530

    Factorial.java

---
+++ src/com/test/easy/Factorial.java
@@ -0,0 +1,11 @@
+package com.test.easy;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 5, Factorial = 1;
+        for (int i = 1; i<= num; i++) {
+            Factorial = Factorial * i;
+        }
+        System.out.println(Factorial);
+    }
+}
