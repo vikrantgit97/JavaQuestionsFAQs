e8ab2e1132e4efaa125212670dcabd2dc86f7bf5
Vikrant <vikrantgopewar@gmail.com>
2023-08-12 18:03:45 +0530

    FactorsOfNumber.java

---
+++ src/com/test/easy/FactorsOfNumber.java
@@ -0,0 +1,16 @@
+package com.test.easy;
+public class FactorsOfNumber {
+
+    public static void main(String[] args) {
+        int number = 15;
+        int i = 1;
+        while (i <= number) 
+        {
+            if (number % i == 0) //if number is divisible by i or not to find factor
+            {
+                System.out.println(i); 
+            }
+            i++;
+        }
+    }
+}
