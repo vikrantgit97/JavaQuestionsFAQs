6e9bb6943ebe9856d487604c20432d141801b32c
Vikrant <vikrantgopewar@gmail.com>
2023-08-23 23:43:32 +0530

    src/com/test/easy/FibonassiSeries.java

--- src/com/test/easy/FibonassiSeries.java
+++ src/com/test/easy/FibonassiSeries.java
@@ -3,18 +3,18 @@ package com.test.easy;
 // wap a program to print fibonassi series of number 10 i.e. 0 1 1 2 3 5 8 13 21 34  
 public class FibonassiSeries {
 
-	public static void CountWordsFromString(int str) {
+	public static void main(String[] args) {
+		int findFibonassiOf = 10;
 		int a = 0;
 		int b = 1;
-		for (int i = 1; i <= str; i++) {
-			System.out.print(a + " ");
+		// List list=new ArrayList<>();
+		for (int i = 1; i <= findFibonassiOf; i++) {
+			System.out.print(a + ", ");
+			// list.add(a);
 			int c = a + b;
 			a = b;
 			b = c;
 		}
-	}
-
-	public static void main(String[] args) {
-		CountWordsFromString(10);
+		// System.out.println(list);
 	}
 }
