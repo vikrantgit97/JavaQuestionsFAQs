856e568f7623d01c7ecd2c68b22bd3e349d5b88a
Vikrant <vikrantgopewar@gmail.com>
2023-08-23 23:31:46 +0530

    src/com/test/easy/ReverseString.java

---
+++ src/com/test/easy/ReverseString.java
@@ -0,0 +1,14 @@
+package com.test.easy;
+
+public class ReverseString{
+    public static void main(String[] args) {
+        String name="brock";
+        String reverse="";
+        char letters;
+        for(int i =name.length()-1;i>=0;i--){
+            letters=name.charAt(i);
+            reverse=reverse+letters;
+        }
+        System.out.println(reverse);
+    }
+}
