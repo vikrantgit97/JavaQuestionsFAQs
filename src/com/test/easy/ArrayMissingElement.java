db2da2c30c1a08cdf455488f08ff5ce51b01fc36
Vikrant <vikrantgopewar@gmail.com>
2023-09-02 22:14:32 +0530

    src/com/test/easy/ArrayMissingElement.java

---
+++ src/com/test/easy/ArrayMissingElement.java
@@ -0,0 +1,18 @@
+package com.test.easy;
+// find missing number between the sequence
+public class ArrayMissingElement {
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 4, 6, 7, 8 ,9};
+        for (int num = 1; num <= a.length; num++) {
+            int count = 0;
+            for (int j = 0; j < a.length; j++) {
+                if (num == a[j]) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                System.out.println("Missing element is: " + num);
+            }
+        }
+    }
+}
