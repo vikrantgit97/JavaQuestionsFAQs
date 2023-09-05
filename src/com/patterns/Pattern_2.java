236d3e005fe6eff92644d3e3d6b6f4b9519ae2dc
Vikrant <vikrantgopewar@gmail.com>
2023-09-05 23:47:53 +0530

    src/com/patterns/Pattern_2.java

---
+++ src/com/patterns/Pattern_2.java
@@ -0,0 +1,24 @@
+package com.patterns;
+
+public class Pattern_2 {
+    public static void main(String[] args) {
+  // step-1 outer for loop for rows & inner for loop for column
+
+        //          * * * *             row 1
+        //          * * *             row 2
+        //          * *             row 3
+        //          *             row 4
+        pattern(4);
+    }
+
+    public static void pattern(int n) {
+        for (int row = 1; row <= n; row++) {
+            // step-2 identify for every row how many column are there
+            for (int column = 1; column <= n-row+1; column++) {
+                System.out.print(" * ");
+            }
+            // step-3 when new row printed add new line
+            System.out.println();
+        }
+    }
+}
