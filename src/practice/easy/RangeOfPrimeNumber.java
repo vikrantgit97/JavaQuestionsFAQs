f088d8822f7638603c782e562b134050b9ad0089
Vikrant <vikrantgopewar@gmail.com>
2023-11-04 22:35:17 +0530

    range of prime number

---
+++ src/practice/easy/RangeOfPrimeNumber.java
@@ -0,0 +1,17 @@
+package practice.easy;
+
+/**
+ * @author Vikrant on 04-11-2023
+ * @version V1.0
+ * @Project JavaQuestionsFAQs
+ */
+public class RangeOfPrimeNumber {
+    public static void main(String[] args) {
+
+        int count=0, start=5, end=10;
+
+        for(int i=start; i<=end; i++){
+            if(i)
+        }
+    }
+}
