f67f09b222001ec887cf42684de2d84d7cd7452e
Vikrant <vikrantgopewar@gmail.com>
2023-11-27 20:53:40 +0530

    src/practice/easy/FactorOfNumber.java

--- src/practice/easy/FactorOfNumber.java
+++ src/practice/easy/FactorOfNumber.java
@@ -1,14 +1,14 @@
 package practice.easy;
+
 public class FactorOfNumber {
 
     public static void main(String[] args) {
         int number = 15;
         int i = 1;
-        while (i <= number) 
-        {
+        while (i <= number) {
             if (number % i == 0) //if number is divisible by i or not to find factor
             {
-                System.out.println(i); 
+                System.out.println(i);
             }
             i++;
         }
