0a425949858e34a07525006650d5b0375d5637af
Vikrant <vikrantgopewar@gmail.com>
2024-02-14 11:33:50 +0530

    src/practice/kk/MathForDsa2/PrimeNumber.java

--- src/practice/kk/MathForDsa2/PrimeNumber.java
+++ src/practice/kk/MathForDsa2/PrimeNumber.java
@@ -25,6 +25,13 @@ public class PrimeNumber {
             }
             c++;
         }
+
+        // for (int i = 2; i * i <= n; i++) {
+        //     if (n % i == 0) {
+        //         return false;
+        //     }
+        // }
+
         return true;
     }
 }
