63cafab876d4e5606a962722d604351e631ef23e
Vikrant <vikrantgopewar@gmail.com>
2024-02-15 17:53:32 +0530

    src/practice/interviewquestion/FindDuplicatesTriplicatesFromArray.java

---
+++ src/practice/interviewquestion/FindDuplicatesTriplicatesFromArray.java
@@ -0,0 +1,35 @@
+package practice.interviewquestion;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class FindDuplicatesTriplicatesFromArray {
+    public static void main(String[] args) {
+        int arr[] = { 5, 5, 1, 1, 1, 6, 6, 0 };
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        System.out.println(map);
+
+        //retrurn element which is duplicate or more than that
+        //java 8
+        map.entrySet().
+                stream().
+                    filter(x->x.getValue()>=2).
+                        map(Map.Entry::getKey).
+                            forEach(System.out::println);
+
+        //java 7
+        Set<Integer> s = map.keySet();
+        for (int i : s) {
+            if (map.get(i) >= 2) {
+                System.out.print(i + " ");
+            }
+        }
+
+
+
+    }
+}
