206a8bd4410908f7ebd3584f6d6eb5e8cb9f14db
Vikrant <vikrantgopewar@gmail.com>
2024-02-20 21:19:00 +0530

    modified : src/practice/easy/Factorial.java

--- src/practice/easy/Factorial.java
+++ src/practice/easy/Factorial.java
@@ -4,7 +4,7 @@ public class Factorial {
     public static void main(String[] args) {
         int num = 5, Factorial = 1;
         for (int i = 1; i<= num; i++) {
-            Factorial = Factorial * i;
+            Factorial *= i;
         }
         System.out.println(Factorial);
     }
