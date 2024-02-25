a44814345927351edcd24cb846d5d80e7e613506
Vikrant <vikrantgopewar@gmail.com>
2024-02-25 09:17:02 +0530

    src/practice/kk/BinarySearch/SortedMatrix.java to be solve

---
+++ src/practice/kk/BinarySearch/SortedMatrix.java
@@ -0,0 +1,12 @@
+package practice.kk.BinarySearch;
+
+
+public class SortedMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+    }
+}
